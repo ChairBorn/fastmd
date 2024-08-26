ec7d9799cdb2baa6201cc926cb798da6fc69502e
ChairBorn <mysakyuriy@gmail.com>
2024-08-26 12:33:18 -0700

    adjust functions naming
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fasthtml.common import *
  
 app,rt = fast_app()
 @rt('/')
-def get():
+def get_home():
     return Div(P('Hello World!'), hx_get="/change")
 
 @rt('/change')
-def get():
+def get_change():
     return P('nice homie')
 
 serve()
